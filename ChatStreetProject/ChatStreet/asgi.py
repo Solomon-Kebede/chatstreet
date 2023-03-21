c666c372c3f9ed8e13043a6f880685b169a75a06
Solomon Kebede <solomonaau188@gmail.com>
2023-03-21 15:20:00 +0000

    Configure ASGI_APPLICATION

--- ChatStreetProject/ChatStreet/asgi.py
+++ ChatStreetProject/ChatStreet/asgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatStreet.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+	{
+		"http": get_asgi_application(),
+	}
+)
--- ChatStreetProject/ChatStreet/settings.py
+++ ChatStreetProject/ChatStreet/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ChatStreet.wsgi.application'
-
+ASGI_APPLICATION = 'ChatStreet.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
