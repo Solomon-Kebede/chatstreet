6e7f1018ad7e9e32d1dbb8c8939fc2fb50d09cf3
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-21 22:01:15 +0300

    set the home page as index

--- ChatStreetProject/ChatStreet/urls.py
+++ ChatStreetProject/ChatStreet/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chat/', include('chatapp.urls')),
+    path('', include('chatapp.urls')),
 ]
