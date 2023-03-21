9094ba7f46a1b7ef011447198486d566805cab68
Solomon Kebede <solomonaau188@gmail.com>
2023-03-21 15:24:34 +0000

    Fix view render

--- ChatStreetProject/chatapp/views.py
+++ ChatStreetProject/chatapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-	return render(request, render('chat/rooms/index.html'))
+	return render(request, 'chat/rooms/index.html')
