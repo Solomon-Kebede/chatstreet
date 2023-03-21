79f1b794fa0bbb1aa4fc8bf69cff5b68e9135e1d
Solomon Kebede <solomonaau188@gmail.com>
2023-03-21 15:10:10 +0000

    Change project settings and add a new chatroom view

--- ChatStreetProject/ChatStreet/asgi.py
+++ ChatStreetProject/ChatStreet/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for ChatStreetProject project.
+ASGI config for ChatStreet project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatStreetProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatStreet.settings')
 
 application = get_asgi_application()
