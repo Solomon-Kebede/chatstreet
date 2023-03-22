a8acc7bc592053520b73af55d0cedc6040893823
Solomon Kebede <solomonaau188@gmail.com>
2023-03-22 07:50:07 +0000

    Add Websockets

--- ChatStreetProject/ChatStreet/asgi.py
+++ ChatStreetProject/ChatStreet/asgi.py
@@ -8,13 +8,22 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatStreet.settings')
 
+django_asgi_app = get_asgi_application()
+
+import chatapp.routing
+
 application = ProtocolTypeRouter(
 	{
-		"http": get_asgi_application(),
+		"http": django_asgi_app,
+		"websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chatapp.routing.websocket_urlpatterns))
+        ),
 	}
 )
