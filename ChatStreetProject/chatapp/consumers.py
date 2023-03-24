d73ddd20c2f331698a351475b248fbdc3c6d0b41
Solomon Kebede <solomonaau188@gmail.com>
2023-03-24 00:35:27 +0000

    Positioning chat message timestamps

--- ChatStreetProject/chatapp/consumers.py
+++ ChatStreetProject/chatapp/consumers.py
@@ -21,18 +21,29 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        client = text_data_json["client"]
+        message = text_data_json.get("message")
+        client = text_data_json.get("client")
+        timestamp = text_data_json.get("timestamp")
 
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat_message", "message": message, "client": client}
+            self.room_group_name, {
+            "type": "chat_message",
+            "message": message,
+            "client": client,
+            "timestamp": timestamp,
+            }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event["message"]
-        client = event["client"]
+        message = event.get("message")
+        client = event.get("client")
+        timestamp = event.get("timestamp")
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message, "client": client}))
+        await self.send(text_data=json.dumps({
+            "message": message,
+            "client": client,
+            'timestamp': timestamp,
+            }))
