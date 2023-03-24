f351dcb6b7319c1e726193b248e2f89ca2f4bbb0
Solomon Kebede <solomonaau188@gmail.com>
2023-03-24 02:07:19 +0000

    Fix redirection issue

--- ChatStreetProject/chatapp/urls.py
+++ ChatStreetProject/chatapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('index/', views.index, name="index"),
-	#path('chatroom/', views.chatRoom, name="chatroom"),
-	path("<str:room_name>/", views.chatRoom, name="chatroom"),
+	path('chatroom/', views.chatRoom, name="chatroom"),
+	# path("<str:room_name>/", views.chatRoom, name="chatroom"),
 ]
--- ChatStreetProject/chatapp/views.py
+++ ChatStreetProject/chatapp/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'chat/index.html')
 
-def chatRoom(request, room_name):
-    return render(request, 'chat/room.html', {"room_name": room_name})
+def chatRoom(request):#, room_name):
+    return render(request, 'chat/room.html')#, {"room_name": room_name})
