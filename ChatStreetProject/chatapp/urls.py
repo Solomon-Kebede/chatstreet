c84b8c19e8afa006e802107f252e72e1000cf581
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2023-03-28 12:56:51 +0300

    logout button implementation

--- ChatStreetProject/chatapp/urls.py
+++ ChatStreetProject/chatapp/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
 	path('index/', views.index, name="index"),
 	path('chatroom/', views.chatRoom, name="chatroom"),
+	path('logout/', auth_views.LogoutView.as_view(next_page='loginaccount'), name="logout")
 	# path("<str:room_name>/", views.chatRoom, name="chatroom"),
 
 ]
